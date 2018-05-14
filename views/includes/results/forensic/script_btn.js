48afed6995ac37eee3690513f7963217aecc5954
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2018-05-14 14:51:37 +0000

    @ancoar feedback

--- views/includes/results/forensic/script_btn.js
+++ views/includes/results/forensic/script_btn.js
@@ -20,17 +20,19 @@
   * gestion de renderizado de diferentes fotos para forense
   * al pulsar diferentes botones
 */
-const extension = ['.jpg', '.png','.tiff', '.gif', '.bmp'];
-let archive = "cucu.txt",
-    newArchive = '',
+const path = 'forensic/'
+let urlImgame = document.querySelector(".forensic-image").src
+let //file = urlImgame.split(path)[1],
+    //newPath = '',
     button = document.getElementsByClassName('foresic-element_button');
 
 for (var i = 0; i < button.length; i++) {
   let dataImage = button[i].getAttribute('data-image');
-  button[i].addEventListener('click', function(){
-    let archiveName = archive.split('.');
-    newArchive = archiveName[0] + dataImage + '.' + archiveName[1];
-    console.log(newArchive);
-    return newArchive
+  
+  button[i].addEventListener('click', function(event){
+    //let fileName = file.split('.');
+    //newPath = path + fileName[0] + dataImage + '.png';
+    document.querySelector(".forensic-image").src = event.target.getAttribute("data-src");
   });
+  
 }
